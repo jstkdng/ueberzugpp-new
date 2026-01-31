18222b1fb24d9908baa7e2a73a27787bad2f7403
JustKidding <jk@vin.ovh>
2026-01-31 01:13:56 -0500

    add more wayland stuff

--- include/wayland/base.hpp
+++ include/wayland/base.hpp
@@ -18,23 +18,45 @@
 
 #pragma once
 
+#include "util/str_map.hpp"
 #include "wayland/types.hpp"
+
+#include <any>
 
 namespace upp
 {
+
+struct WaylandGlobal {
+    uint32_t name;
+    uint32_t version;
+};
 
 class WaylandBase
 {
   public:
     WaylandBase();
+    virtual ~WaylandBase() = default;
 
     static void wl_registry_global(void *data, wl_registry *registry, uint32_t name, const char *interface,
                                    uint32_t version);
+    static void xdg_wm_base_ping(void *data, xdg_wm_base *xdg_wm_base, uint32_t serial);
+
     void init();
 
-  protected:
+  private:
     wl::display display;
     wl::registry registry;
+    string_map<WaylandGlobal> globals;
+
+    void bind_base_protocols();
+
+  protected:
+    wl::compositor compositor;
+    wl::shm shm;
+    xdg::wm_base wm_base;
+
+    auto bind_interface(const char *name, uint32_t version, const wl_interface *interface) const -> std::any;
+    virtual void bind_protocols() {};
 };
 
 } // namespace upp
--- src/wayland/base.cpp
+++ src/wayland/base.cpp
@@ -18,6 +18,7 @@
 
 #include "wayland/base.hpp"
 #include "exceptions.hpp"
+#include "util/log.hpp"
 
 namespace upp
 {
@@ -27,10 +28,20 @@ constexpr wl_registry_listener registry_listener = {
     .global_remove = wl::ignore,
 };
 
-void WaylandBase::wl_registry_global(void *data, wl_registry *registry, uint32_t name, const char *interface,
-                                     [[maybe_unused]] uint32_t version)
+constexpr xdg_wm_base_listener xdg_wm_base_listener = {
+    .ping = WaylandBase::xdg_wm_base_ping,
+};
+
+void WaylandBase::wl_registry_global(void *data, [[maybe_unused]] wl_registry *registry, uint32_t name,
+                                     const char *interface, uint32_t version)
 {
-    auto *base = static_cast<WaylandBase*>(data);
+    auto *base = static_cast<WaylandBase *>(data);
+    base->globals.try_emplace(interface, name, version);
+}
+
+void WaylandBase::xdg_wm_base_ping([[maybe_unused]] void *data, xdg_wm_base *xdg_wm_base, uint32_t serial)
+{
+    xdg_wm_base_pong(xdg_wm_base, serial);
 }
 
 WaylandBase::WaylandBase() :
@@ -42,10 +53,34 @@ WaylandBase::WaylandBase() :
     registry.reset(wl_display_get_registry(display.get()));
 }
 
+auto WaylandBase::bind_interface(const char *name, uint32_t version, const wl_interface *interface) const -> std::any
+{
+    auto global = globals.at(name);
+    if (version > global.version) {
+        LOG_WARN("requested version for {} not available, max version available is {}", name, global.version);
+    }
+    return wl_registry_bind(registry.get(), global.name, interface, std::min(version, global.version));
+}
+
 void WaylandBase::init()
 {
     wl_registry_add_listener(registry.get(), &registry_listener, this);
     wl_display_roundtrip(display.get());
+    bind_base_protocols();
+    bind_protocols(); // must be overriden in child classes
+}
+
+void WaylandBase::bind_base_protocols()
+{
+    const uint32_t compositor_ver = 6;
+    const uint32_t shm_ver = 1;
+    const uint32_t xdg_base_ver = 2;
+
+    compositor.reset(
+        std::any_cast<wl_compositor *>(bind_interface("wl_compositor", compositor_ver, &wl_compositor_interface)));
+    shm.reset(std::any_cast<wl_shm *>(bind_interface("wl_shm", shm_ver, &wl_shm_interface)));
+    wm_base.reset(std::any_cast<xdg_wm_base *>(bind_interface("xdg_wm_base", xdg_base_ver, &xdg_wm_base_interface)));
+    xdg_wm_base_add_listener(wm_base.get(), &xdg_wm_base_listener, nullptr);
 }
 
 } // namespace upp
